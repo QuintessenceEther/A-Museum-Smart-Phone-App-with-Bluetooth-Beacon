941bcbc53c394a6ec5c216f91499896052b009ed
Ether <44118457+QuintessenceEther@users.noreply.github.com>
2022-07-11 14:33:13 +0100

    Add files via upload

---
+++ Web management system/Project/src/test/java/com/kai/MuseumApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kai;
+
+import com.kai.utils.MybatisUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuseumApplicationTests {
+
+    @Test
+    public void test(){
+
+    }
+
+}
